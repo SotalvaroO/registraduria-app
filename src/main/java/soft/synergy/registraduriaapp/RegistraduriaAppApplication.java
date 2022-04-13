91b3acfe84c7b68220939f5129b82f44c7a29cba
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-04-13 12:03:32 -0500

    project creation

---
+++ src/main/java/soft/synergy/registraduriaapp/RegistraduriaAppApplication.java
@@ -0,0 +1,13 @@
+package soft.synergy.registraduriaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistraduriaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistraduriaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soft/synergy/registraduriaapp/RegistraduriaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package soft.synergy.registraduriaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistraduriaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
