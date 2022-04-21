5b6113933b5f51d898777be64e71d73058aca3c5
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-04-20 19:57:32 -0500

    Error handler

--- src/main/java/soft/synergy/registraduriaapp/report/models/dtos/PollingLogsRequestDto.java
+++ src/main/java/soft/synergy/registraduriaapp/report/models/dtos/PollingLogsRequestDto.java
@@ -2,6 +2,10 @@ package soft.synergy.registraduriaapp.report.models.dtos;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -9,10 +13,14 @@ import lombok.*;
 @Builder
 public class PollingLogsRequestDto {
 
+    @NotBlank(message = "El codigo del lugar de votacion es obligatorio")
     private String pollingStationCode;
 
+    @NotBlank(message = "El puesto de votacion es obligatorio")
     private String standCode;
 
+    @Positive(message = "La cantidad de votos debe ser mayor a cero")
+    @NotNull(message = "La cantidad de votos es obligatoria")
     private long totalPolls;
 
 }
